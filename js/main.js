c24a8a37e8a4946c9c826d32e441a664a7b8faff
Asiject <oj_only@naver.com>
2022-06-04 21:59:50 +0900

    done

---
+++ js/main.js
@@ -0,0 +1,49 @@
+import {setSearchFocus, showClearTextButton, clearSearchText, clearPushListener} from "./searchBar.js";
+import {buildSearchResults, clearStatsLine, setStatsLine, deleteSearchResults} from "./searchResults.js";
+import {getSearchTerm, retrieveSearchResults} from "./dataFunctions.js";
+
+document.addEventListener("readystatechange", event => {
+  if (event.target.readyState === "complete") {
+    initApp();
+  }
+});
+
+const initApp = () => {
+  // set the focus
+  setSearchFocus();
+  // 3 listeners
+  const search = document.getElementById("search");
+  search.addEventListener("input", showClearTextButton);
+  const clear = document.getElementById("clear");
+  clear.addEventListener("click", clearSearchText);
+  clear.addEventListener("keydown", clearPushListener);
+  const form = document.getElementById("searchBar");
+  form.addEventListener("submit", submitTheSearch);
+};
+
+// procedural 'workflow' function
+const submitTheSearch = event => {
+  event.preventDefault();
+  // delete search results
+  deleteSearchResults();
+  // process the search
+  processTheSearch();
+  // set the focus
+  setSearchFocus();
+};
+
+// procedural
+const processTheSearch = async () => {
+  // clear the stats line
+  clearStatsLine();
+
+  const searchTerm = getSearchTerm();
+  if (searchTerm === "") return;
+  const resultArray = await retrieveSearchResults(searchTerm);
+  // build search results
+  if (resultArray.length) {
+    buildSearchResults(resultArray);
+  }
+  //set stats line
+  setStatsLine(resultArray.length);
+};
